1f5c0ef5dd6ceff6866abc677cb87e8209cc00cc
Roberto Polli <robipolli@gmail.com>
2026-03-10 17:57:36 +0100

    Use external script.

---
+++ create-dataframe.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Loops in assets/controlled-vocabularies to create datapackage.yaml
+#
+: "${1:?Usage: $0 <asset_name>}"
+
+ASSET_NAME="$1"
+
+python -m tools.commands datapackage create \
+    --ttl "${ASSET_NAME}.ttl" \
+    --frame "${FRAME_FILE}.frame.yamlld" \
+    --vocabulary-uri "changeme" \
+    --output "datapackage.yaml" \
+    --lang it \
+    --force
